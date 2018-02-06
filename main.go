47e46a45dffe44cd771c95a1d8ef977e39674d4e
Felipe Netto <flpncc@gmail.com>
2018-02-06 16:41:50 -0300

    Parsing XML data

--- main.go
+++ main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
-func getBodyData(address string) string {
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+type SiteMapIndex struct {
+	Locations []Location `xml:"sitemap"`
+}
+
+func (location Location) String() string {
+	return fmt.Sprintf(location.Loc)
+}
+
+func getBodyData(address string) {
 	response, _ := http.Get(address)
 	bytes, _ := ioutil.ReadAll(response.Body)
 	response.Body.Close()
 
-	return string(bytes)
+	var siteMapIndex SiteMapIndex
+	xml.Unmarshal(bytes, &siteMapIndex)
+
+	fmt.Println(siteMapIndex.Locations)
 }
 
 func main() {
-	siteData := getBodyData("https://www.washingtonpost.com/news-sitemap-index.xml")
-	fmt.Println(siteData)
+	getBodyData("https://www.washingtonpost.com/news-sitemap-index.xml")
 }
